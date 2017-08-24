007cb30a572e7e1893d9d63369af8149a7fb8f75
Minxine-Yao <seuhuangziyao@outlook.com>
2017-08-24 16:35:44 +0800

    Now we can handle most of the undesirable breaks of lines

--- modules/get_poem.py
+++ modules/get_poem.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 –*-
 
+import urllib
 from urllib.request import urlopen
 from urllib.error import  HTTPError
+from urllib.parse import quote
 from bs4 import BeautifulSoup
 
 def getPoem(poem):
@@ -24,7 +26,7 @@ def getPoem(poem):
             comment = bsObj.findAll("div", class_="contyishang")[1].findAll('p')[-1].get_text("\n", strip=True)
     else:
         # 搜索结果页
-        html = urlopen("http://so.gushiwen.org/search.aspx?value="+title)
+        html = urlopen(("http://so.gushiwen.org/search.aspx?value=" + quote(title)))
         bsObj = BeautifulSoup(html.read(), "html.parser")
         linkDiv = bsObj.find("div", class_="sons")
         if linkDiv is not None:
@@ -32,14 +34,46 @@ def getPoem(poem):
             link = "http://so.gushiwen.org/" + linkDiv.a["href"]
             poem_html = urlopen(link)
             bsObj = BeautifulSoup(poem_html.read(), "html.parser")
-            content = bsObj.find("div", class_="contson").p.get_text("\n", strip=True)
-            comment = bsObj.findAll("div", class_="contyishang")[1].findAll('p')[-1].get_text("\n", strip=True)
+            content = bsObj.find("div", class_="contson").get_text("\n", strip=True)
+            try:
+                comment = bsObj.findAll("div", class_="contyishang")[1].findAll('p')[-1].get_text("\n", strip=True)
+            except IndexError:
+                print(title + "注释爬取异常")
+            else:
+                pass
+
 
     poemInfo = {
         "title": title,
         "author": author,
         "content": content,
-        "comment": comment
+        "comment": stripComment(comment)
     }
 
     return poemInfo
+
+def stripComment(cmt):
+    res = cmt
+    if cmt == "暂缺":
+        return res
+
+    cmt = cmt[2:-2]
+    # 遍历注释中的每个字符
+    for pos in range(0,len(cmt)):
+        if pos >= len(cmt):
+            break
+        if cmt[pos] == "\n":
+            # 换行符后跟的不是带括号数字
+            next_chr = ord(cmt[pos+1])
+            if (
+                chr(9312) in cmt and (next_chr > 9331 or next_chr < 9312) or
+                chr(9353) in cmt and (next_chr > 9371 or next_chr < 9353) or
+                "1" in cmt and (next_chr > ord("9") or next_chr < ord("1")) or
+                "[1]" in cmt and next_chr != ord("[") or
+                "（1）" in cmt and next_chr != ord("（") or
+                "【" in cmt and next_chr != ord("【")
+                ):
+                    cmt = cmt[:pos] + cmt[pos+1:]
+                    pos = pos - 1
+
+    return cmt
--- poetry_collector.py
+++ poetry_collector.py
@@ -4,8 +4,12 @@ from modules.get_poem import getPoem
 def main():
     filepath = "catalog.txt"
     catalog = catalogParser(filepath)
-    poem = getPoem(catalog[0])
-    print(poem)
+    poetryPath = "poetry.md"
+    with open(poetryPath, 'w', encoding="utf8") as f:
+        f.write("# Poetry\n")
+        for poem in catalog:
+            poemInfo = getPoem(poem)
+            f.write(("## %s\n**%s**\n> %s\n\n%s\n")%(poemInfo["title"], poemInfo["author"], poemInfo["content"], poemInfo["comment"]))
 
 if __name__ == "__main__":
     main()
